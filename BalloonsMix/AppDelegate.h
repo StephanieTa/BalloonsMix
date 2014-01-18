1d88ba265354a78c30936b08985b55bcaa64bcb7
Stephanie Ta <stephanie.ta@outlook.com>
2014-01-18 23:46:46 +0100

    Initial Commit

---
+++ BalloonsMix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BalloonsMix
+//
+//  Created by Stephanie Ta on 18.01.14.
+//  Copyright (c) 2014 Stephanie Ta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BalloonsMix/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BalloonsMix
+//
+//  Created by Stephanie Ta on 18.01.14.
+//  Copyright (c) 2014 Stephanie Ta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
